1e8f9f99489da9022a74764f10253c1a8e3dca8d
Mohamed Benlmaoujoud <40149539+benlmaoujoud@users.noreply.github.com>
2020-10-23 07:35:58 +0100

    Update Watermelon.java

--- Codeforces/src/Watermelon.java
+++ Codeforces/src/Watermelon.java
@@ -2,22 +2,17 @@ import java.util.Scanner;
 public class Watermelon {
 
 	public static void main(String[] args) {
-		
-		String str;
 		int n;
 		Scanner input = new Scanner(System.in);
 
 		n=input.nextInt();
-		
+		// we have to check if w is even and greather then 2 (because 2 =1+1)
 		if(w%2 == 0 && w > 2) {
 			System.out.println("YES");
 		}
 		else {
 			System.out.println("NO");
 		     }
-		
-		
-		
 		
 	}
 
