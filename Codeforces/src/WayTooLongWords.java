67fab6df750d16dc6adbb628b45defee01cb26c7
Mohamed Benlmaoujoud <40149539+benlmaoujoud@users.noreply.github.com>
2020-10-23 07:50:59 +0100

    Update WayTooLongWords.java

--- Codeforces/src/WayTooLongWords.java
+++ Codeforces/src/WayTooLongWords.java
@@ -1,34 +1,32 @@
 import java.util.Scanner;
 
-class Point {
-	public int x,y;
-
-	public Point( int x, int y) {
-		
-	this.x=x;
-	this.y=y;
-	
-	}
-	
-	
-}
-
 public class WayTooLongWords {
-	
-		public static void main(String args[])
-		{
-			Point y = new Point(2,4);
-			Point z = new Point(2,4);
-			boolean s=coin(y, z);
-			System.out.println(s);
-			
-		}
-		
-		public  static boolean coin(Point a,Point b)
-		{
-			return((a.x==b.x) && (a.y==b.y));
-			
-		}
-		
-		
-	}
+
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        int n;
+        n=input.nextInt();
+	// n is the number of words 
+        String[] words=new String[n+1];
+        for(int i=0;i<n+1;i++)
+        {
+            words[i]=input.nextLine();
+        }
+        for(int i=0;i<n+1;i++)
+        {
+            if(words[i.length()>10)
+            {
+		//charAt() is an inbuilt method which can read a string letter by letter
+		// for exemple let words="Hello" , then words.charAt(0)="H"
+                System.out.print(words[i].charAt(0));
+                System.out.print(words[i].length()-2);
+                System.out.print(words[i].charAt(words[i].length()-1));
+            }
+	// we must just to print the word if wordlngth<10
+		System.out.println(word[i]);
+
+            System.out.print("\n");
+        }
+    }
+}
